91e082a5b7ca4f0a11ec6bef4389c8745035cbac
Divine Aseaku <divineaseaku@gmail.com>
2022-01-02 17:03:14 -0500

    neon.js
    finished work on all projects page

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,5 @@
+
+//for carousel
 const track = document.querySelector('.carousel_track');
 const slides = Array.from(track.children);
 const nextButton = document.querySelector('.carousel_button--right');
@@ -7,6 +9,93 @@ const dots = Array.from(carouselNav.children);
 
 const slideWidth = slides[0].getBoundingClientRect().width;
 
-//arrange the slides next to each other
-slide[0].style.left = 0;
-slide[1].style.left = slideWidth + 'px';
+//arrange the slides next to each other with a loop
+slides.forEach((slide, index) => {
+  slide.style.left = slideWidth * index + 'px';
+});
+
+//update the dots
+const updateDots = (currentDot, targetDot )=> {
+  currentDot.classList.remove('active-slide');
+  targetDot.classList.add('active-slide');
+}
+//hide or show the prev and next buttons
+const updateArrowButtons = (slides, prevButton, nextButton, targetIndex ) => {
+  if(targetIndex ===0){
+    prevButton.classList.add('disabled');
+    nextButton.classList.remove('disabled');
+  }else if(targetIndex === slides.length -1){
+    nextButton.classList.add('disabled');
+    prevButton.classList.remove('disabled');
+  }else{
+    nextButton.classList.remove('disabled');
+    prevButton.classList.remove('disabled');
+  }
+}
+
+//move the slide to the left
+nextButton.addEventListener('click', e => {
+  //get the current slide
+  const currentSlide = track.querySelector('.active-slide');
+  //get the next slide
+  const nextSlide = currentSlide.nextElementSibling;
+  const amountToMove = nextSlide.style.left;
+  //move to next slide
+  track.style.transform = 'translateX(-' + amountToMove + ')';
+  //update the dots
+  const currentDot = carouselNav.querySelector('.active-slide');
+  const nextDot = currentDot.nextElementSibling;
+  const nextIndex = slides.findIndex(slide => slide === nextSlide); 
+
+  updateArrowButtons(slides, prevButton, nextButton, nextIndex);
+  updateDots(currentDot, nextDot);
+  //update the active slide
+  currentSlide.classList.remove('active-slide');
+  nextSlide.classList.add('active-slide');
+});
+
+//move the slide to the right
+prevButton.addEventListener('click', e => {
+  //get the current slide
+  const currentSlide = track.querySelector('.active-slide');
+  //get the next slide
+  const prevSlide = currentSlide.previousElementSibling;
+  const amountToMove = prevSlide.style.left;
+  //move to next slide
+  track.style.transform = 'translateX(-' + amountToMove + ')';
+  //update the dots
+  const currentDot = carouselNav.querySelector('.active-slide');
+  const prevDot = currentDot.previousElementSibling;
+  const prevIndex = slides.findIndex(slide => slide === prevSlide);
+
+  updateDots(currentDot, prevDot);
+  //update the active slide
+  currentSlide.classList.remove('active-slide');
+  prevSlide.classList.add('active-slide');
+  updateArrowButtons(slides, prevButton, nextButton, prevIndex);
+
+});
+
+// set up the dots and 
+carouselNav.addEventListener('click', e => {
+  const targetDot = e.target.closest('button');
+  if (!targetDot) return;
+  const currentSlide = track.querySelector('.active-slide');
+  const currentDot = carouselNav.querySelector('.active-slide');
+  const targetIndex = dots.findIndex(dot => dot === targetDot);
+  const targetSlide = slides[targetIndex];
+  const amountToMove = targetSlide.style.left;
+  //move to next slide
+  track.style.transform = 'translateX(-' + amountToMove + ')';
+  //update the active slide
+  currentSlide.classList.remove('active-slide');
+  targetSlide.classList.add('active-slide');
+ // update dots
+  updateDots(currentDot, targetDot);
+  updateArrowButtons(slides, prevButton, nextButton, targetIndex);
+
+});
+
+
+
+
