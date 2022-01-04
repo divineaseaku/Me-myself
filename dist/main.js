fbbc2bda8917f963b594fc238a59e7ff4bc8cb8e
Divine Aseaku <divineaseaku@gmail.com>
2022-01-04 00:12:06 -0500

    main.js

--- dist/main.js
+++ dist/main.js
@@ -96,6 +96,15 @@ carouselNav.addEventListener('click', e => {
 
 });
 
+// for side-menu
+function openSlideMenu(){
+  document.getElementById('side-menu').style.width = '290px';
+  document.getElementById('side-menu').style.display = 'block';
+   
+}
+  function closeSlideMenu(){
+    document.getElementById('side-menu').style.width = '0px';
+  }
 
 
 
