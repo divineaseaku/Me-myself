2448e70a34f1ac3e895b111e2e086ed6b04b3cad
Divine Aseaku <divineaseaku@gmail.com>
2022-01-14 20:17:46 -0500

    projects

--- dist/projects/happyDay/script/main.js
+++
@@ -1,8 +0,0 @@
-function openSlideMenu(){
-  document.getElementById('side-menu').style.width = '290px';
-  document.getElementById('side-menu').style.display = 'block';
-   
-}
-  function closeSlideMenu(){
-    document.getElementById('side-menu').style.width = '0px';
-  }
